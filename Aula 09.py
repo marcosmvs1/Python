fc4b75660697b1119a59931a736501592424f9b7
Marcos Vieira <145136000+marcosmvs1@users.noreply.github.com>
2023-10-12 12:39:57 -0300

    continuiaAula9

---
+++ Aula 09.py
@@ -0,0 +1,26 @@
+frase= "Curso em Video Python"
+
+#"[:5]"exibe os caracteres conforme o numero foi inserido ex:
+print(frase[:3])
+
+#"Len" informa o comprimento da frase ex:
+print(len(frase))
+
+#conta a quantidade que a letra solicitada aparece na frase ex:
+print(frase.count('o'))
+
+#procura determinda palavra na frase, informando a posição que começa a palavra solicitada ex:
+print(frase.find('deo'))
+
+#informa se contem ou não uma palavra em uma frase (True or False )ex:
+print('Marcos' in frase)
+print('Curso' in frase)
+
+#como substituir uma palavra em um frase 
+print(frase.replace('Curso', 'Aula '))
+
+#função upper() trasforma letras minusculas em maiusculas
+print(frase.upper())
+
+#função lower() é contrário do upper, transforma as letras maiusculas em minúsculas 
+print(frase.lower())
