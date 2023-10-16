27a00389a4a7c957936852ca290419f906d9e902
Marcos Vieira <145136000+marcosmvs1@users.noreply.github.com>
2023-10-16 11:37:39 -0300

    Update ex022.py

--- ex022.py
+++ ex022.py
@@ -1,3 +1,10 @@
-nome = str(input('Digite seu nome:'))
-print(f'Seu nome em maiúscula fica assim: {nome.upper()}' )
-print(f'Seu nome em minuscula fica assim: {nome.lower()}')
+nome = str(input('Digite seu nome:')).strip()
+print(f'Seu nome em maiúscula fica assim: {nome.upper()}')
+print(f'Seu nome em minúscula fica assim: {nome.lower()}')
+print('Seu nome tem ao todo {} letras' .format(len(nome)-nome.count(' ')))
+print('Seu primeiro nome tem {} letras' .format(nome.find(' ')))
+separa = nome.split()
+print(f'Seu primeiro nome é {separa[0]} e tem {len(separa[0])} letras')
+
+
+
