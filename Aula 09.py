7df0d5a12fce8f547a0ea8277e464477c22d43e5
Marcos Vieira <145136000+marcosmvs1@users.noreply.github.com>
2023-10-16 09:51:01 -0300

    Update Aula 09.py

--- Aula 09.py
+++ Aula 09.py
@@ -27,3 +27,25 @@ print(frase.lower())
 
 #Função captalize() altera a somente PRIMEIRA letra do texto para maiuscula ex:
 print(frase.capitalize())
+
+#Função title() procupra palavra por palavra e transforoma a priemira letra em maiúscula 
+print(frase.title())
+
+#Função strip() ajusta(anula) os espaços do começo e do fim da frase caso exista ex:
+frase2 = '   Aprenda python  '
+print(frase2.strip())
+
+#função rstrip() ajusta os espaços apenas do fim da frase 
+print(frase2.rstrip())
+
+#função lstrip() ajusta os espaços apenas do começo da frase 
+print(frase2.lstrip())
+
+#função .split() divide cada palavra da frase em uma lista com todas as palavras de uma cadeia de caracteres
+dividido = frase.split()
+
+#função .join() junta as palavras de uma frase separando-as por qualquer caracter desejado, basta colocar o caracter desejado em entre aspas antes do comando .join() ex:
+print('-'.join(dividido))
+
+print(dividido.split())
+
