418db054136cb1ab76f2cc81720995a44e33bf4b
Marcos Vieira <145136000+marcosmvs1@users.noreply.github.com>
2023-11-24 19:41:06 -0300

    teste

--- Curso de python/Aulas/Aula 06,1.py
+++ Curso de python/Aulas/Aula 06,1.py
@@ -1,2 +1,18 @@
-n1 = input('Digite um valor: ')
-print(type(n1))
+while True:
+    entrada_numerica = input('Digite um valor numérico: ')
+
+    if entrada_numerica.isdigit():
+        print('Parabéns! Você digitou números corretamente.')
+        
+        while True:
+            nome = input('Agora digite seu nome: ')
+            
+            if nome.isalpha():
+                print('Obrigado, {}!'.format(nome))
+                break
+            else:
+                print('Você precisa digitar apenas letras. Tente novamente.')
+
+        break
+    else:
+        print('Você precisa digitar números. Tente novamente.')
