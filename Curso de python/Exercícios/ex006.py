6737b6fd813cd647d50254d3187ad452d91793e4
Marcos Vieira <145136000+marcosmvs1@users.noreply.github.com>
2023-11-24 21:04:42 -0300

    24/11

---
+++ Curso de python/Exercícios/ex006.py
@@ -0,0 +1,7 @@
+num = int (input('Digite um número: '))
+dobro = num * 2
+triple = num * 3
+raiz = num**(1/2)
+print(f'O dobro de {num} é {dobro}')
+print(f'O triplo de {num} é {triple}')
+print(f'A raiz quadrada de {num} é {raiz :.1F}')
---
+++ Curso de python/Exercícios/ex007.py
@@ -0,0 +1,4 @@
+nota1 = int (input('Digite a nota da primeira prova: '))
+nota2 = int (input('Digite a nota da segunda prova: '))
+media = (nota1 + nota2) / 2
+print(f'A média do aluno foi de {media :.1F}')
