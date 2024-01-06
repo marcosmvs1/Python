f377e1c3ec47ac73a21591d1403cf6cb48f43267
Marcos Vieira <145136000+marcosmvs1@users.noreply.github.com>
2024-01-06 13:23:58 -0300

    ex60

--- Curso de python/Exercícios/ex060.py
+++ Curso de python/Exercícios/ex060.py
@@ -1,7 +1,11 @@
-x  = int (input('Digite um número para calcular seu Fatorial:'))
-
-cont = x
-
-while cont == x:
-    fat = x * 
-
+n = int(input('Digite um número: '))
+c = n 
+f = 1
+print(f'Calculando {n}! >>>', end='')
+while c > 0:
+    print(f' {c} ' , end='')
+    print('x' if c > 1 else '=' , end='')
+    f = f * c 
+    c = c - 1
+    
+print(f' {f} ')
