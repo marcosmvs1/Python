c0316bd5d23e56efab561fb237146e0c72f57a9d
Marcos Vieira <145136000+marcosmvs1@users.noreply.github.com>
2024-02-07 08:43:13 -0300

    te4ste

--- Curso de python/Exercícios/testes.py
+++ Curso de python/Exercícios/testes.py
@@ -1,21 +1,29 @@
-print('Gerador de PA')
-print('-=' * 10)
-primeiro = int(input('Primero termo: '))
-razão = int(input('Razão da PA: '))
-termo = primeiro
-cont = 1
-mais = 10
-while mais != 0:
-    while cont <= mais:
-        print(f' {termo}  ', end= '')
-        
-        cont += 1
-        termo += razão
-    print('Pausa')
-    mais = int(input('Quantos termos você quer mostrar a mais: ')) 
-    
+import random
 
-print('FIM!!!')   
-    
+def jogo_de_adivinhacao():
+    print("Bem-vindo ao jogo de adivinhação!")
+    print("Tente adivinhar um número entre 1 e 10.")
+    print("Você tem uma chance de 40% de acertar.")
 
+    numero_secreto = random.randint(1, 10)
 
+    tentativas = 3
+    chance_acerto = 40
+
+    while tentativas > 0:
+        print("\nTentativas restantes:", tentativas)
+        palpite = int(input("Digite sua aposta (entre 1 e 10): "))
+
+        if palpite == numero_secreto:
+            print("Parabéns! Você acertou!")
+            return
+        elif random.randint(1, 100) <= chance_acerto:
+            print("Você errou! O número era", numero_secreto)
+            return
+        else:
+            print("Você errou! Tente novamente.")
+            tentativas -= 1
+
+    print("\nVocê usou todas as tentativas. O número secreto era", numero_secreto)
+
+jogo_de_adivinhacao()
