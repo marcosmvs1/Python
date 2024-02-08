7414ec8514563ec11b4de7ed041b08594bc90cc2
Marcos Vieira <145136000+marcosmvs1@users.noreply.github.com>
2024-02-08 11:55:20 -0300

    ex064

--- Curso de python/Exercícios/testes.py
+++ Curso de python/Exercícios/testes.py
@@ -1,29 +1,18 @@
-import random
+#Programa pra gerar uma sequencia fibonacci
+print( '-' *30)
+print('      SEQUÊNCIA FIBONACCI')
+print('-' *30)
+n = int(input('Quanto termos você quer mostrar: '))
 
-def jogo_de_adivinhacao():
-    print("Bem-vindo ao jogo de adivinhação!")
-    print("Tente adivinhar um número entre 1 e 10.")
-    print("Você tem uma chance de 40% de acertar.")
-
-    numero_secreto = random.randint(1, 10)
-
-    tentativas = 3
-    chance_acerto = 40
-
-    while tentativas > 0:
-        print("\nTentativas restantes:", tentativas)
-        palpite = int(input("Digite sua aposta (entre 1 e 10): "))
-
-        if palpite == numero_secreto:
-            print("Parabéns! Você acertou!")
-            return
-        elif random.randint(1, 100) <= chance_acerto:
-            print("Você errou! O número era", numero_secreto)
-            return
-        else:
-            print("Você errou! Tente novamente.")
-            tentativas -= 1
-
-    print("\nVocê usou todas as tentativas. O número secreto era", numero_secreto)
-
-jogo_de_adivinhacao()
+t1 = 0 
+t2 = 1
+print('~' * 30)
+print(f'{t1}  -  {t2} ' , end='')
+cont = 3
+while cont <= n:
+    t3 = t1 + t2 
+    print(f' - {t3} ', end='')
+    t1 = t2 
+    t2 = t3
+    cont = cont + 1
+print(' - FIM')
