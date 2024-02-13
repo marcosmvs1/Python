d23524b353a22d30add562514874cb3f66e19fe0
Marcos Vieira <145136000+marcosmvs1@users.noreply.github.com>
2024-02-13 12:29:51 -0300

    teste

--- Curso de python/Exercícios/testes.py
+++ Curso de python/Exercícios/testes.py
@@ -1,2 +1,37 @@
+# Exercicio par ou impar 
 
+from random import randint
+
+v = 0
+while True:
+    print('-=' *20)
+    print('VAMOR JOGAR PAR OU ÍMPAR ')
+    print('-=' *20)
+    jogador = int(input('Digite um número: '))
+    comp = randint(1,10)
+    total = comp + jogador
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('Par ou impar ')).lower() .upper() [0]
+    print(f'Você jogou {jogador} e o computador {comp}. Total de {total}')
+    print('DEU PAR' if total % 2 == 0  else 'DEU IMPAR') 
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Você venceu!!! ')
+            v = v + 1
+        else:
+            print('Você perdeu!!!!')
+            break
+    elif tipo == 'I':
+        if total % 2 != 0:
+            print('Você venceu!!! ')
+            v = v + 1
+        else:
+            print('Você perdeu!!!')
+            break
+    print('Vamos jogar novamnte...')
+print(f'GAME OVER!!! Você venceu  {v} vezes')
+        
+
+   
 
