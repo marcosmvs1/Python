3419f9fb54bb7df0419016ec2ca120ec2c89e2fe
Marcos Vieira <145136000+marcosmvs1@users.noreply.github.com>
2024-02-12 22:35:33 -0300

    ex038
    Aprendendo break no while

--- Curso de python/Exercícios/ex068.py
+++ Curso de python/Exercícios/ex068.py
@@ -1,22 +1,36 @@
 # Exercicio par ou impar 
 
-import random
+from random import randint
 
-print('=-'*15)
-print('VAMOS JOGAR PAR OU ÍMPAR')
-print('=-'*15)
-
+v = 0
 while True:
-    valor = int(input('Diga um valor: ')) 
-    #opção = str(input('Par ou ímpar? [P/I]')) .upper() .lower() [0]
-    comp = random.randint(1,10)
-    print(comp)
-    valor = valor + comp
-    print(valor)
-    resto = valor % 2
+    print('-=' *20)
+    print('VAMOR JOGAR PAR OU ÍMPAR ')
+    print('-=' *20)
+    jogador = int(input('Digite um número: '))
+    comp = randint(1,10)
+    total = comp + jogador
+    tipo = ' '
+    while tipo not in 'PI':
+        tipo = str(input('Par ou impar ')).lower() .upper() [0]
+    print(f'Você jogou {jogador} e o computador {comp}. Total de {total}')
+    print('DEU PAR' if total % 2 == 0  else 'DEU IMPAR') 
+    if tipo == 'P':
+        if total % 2 == 0:
+            print('Você venceu!!! ')
+            v = v + 1
+        else:
+            print('Você perdeu!!!!')
+            break
+    elif tipo == 'I':
+        if total % 2 != 0:
+            print('Você venceu!!! ')
+            v = v + 1
+        else:
+            print('Você perdeu!!!')
+            break
+    print('Vamos jogar novamnte...')
+print(f'GAME OVER!!! Você venceu  {v} vezes')
+        
 
-    if resto == 0:
-        break
-    else:
-        print('-'*30)
-print(f'PROGRAMA TABUADA ENCERRAD0. Volte sempre! ')
+   
